c0c3d1cf2de5d6ebf4c98ab5decc0e7e97f2edcc
Ishtiaq uddin <ishty119@gmail.com>
2022-04-17 17:01:56 +0600

    service section

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,15 +1,35 @@
-import React, { useEffect, useState } from 'react';
+
 import Service from '../Service/Service';
 import './Services.css'
+import service1 from '../../../Images/services/1.png'
+import service2 from '../../../Images/services/2.png'
+import service3 from '../../../Images/services/3.jpg'
 const Services = () => {
+    const services = [
+        {
+            id: 1,
+            name: "Learn Programmin",
+            price: "$1000",
+            description: "learn basic basic programming concept with C programming and  C++",
+            img: service1
+        },
+        {
+            id: 2,
+            name: "Learn Physics",
+            price: "$200",
+            description: "learn fundamental concept of physics for class 11-12. also clear mathemetical concept",
+            img: service2
+        },
+        {
+            id: 3,
+            name: "Learn Mathematic",
+            price: "$250",
+            description: "Class 1-12, Admission Test, Universities and Job Test Preparation Special Courses, Model Tests and Full Directions for mathematic",
+            img: service3
+        }
+    ]
 
-    const [services, setServices] = useState([]);
-
-    useEffect(()=>{
-        fetch('services.json')
-        .then(res => res.json())
-        .then(data => setServices(data))
-    },[])
+    
 
     return (
         <div id="services" className='container'>
